8c57f20efbe7038ad68535a16fb81db96408b797
Speykious <speykious@gmail.com>
2022-02-09 16:33:11 +0100

    Create quickstart Maven project

---
+++ databased/src/test/java/com/based/AppTest.java
@@ -0,0 +1,18 @@
+package com.based;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * VERY rigorous Test :)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
