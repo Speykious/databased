6d2ac5446d38da81824b56191b78ee5004dc4689
Speykious <speykious@gmail.com>
2022-04-06 17:19:20 +0200

    Test is mostly successful
    The machines are not saying yes.
    I think the problem lies in how I read the response.

--- src/main/java/com/based/app/DistributedTestEndpoint.java
+++ src/main/java/com/based/app/DistributedTestEndpoint.java
@@ -14,20 +14,67 @@ import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 public class DistributedTestEndpoint {
 	@GET
 	@Path("/say-yes")
+	@Produces(MediaType.TEXT_HTML)
 	public String sayYes() {
 		return "yes";
 	}
 
 	@GET
 	@Path("/broadcast")
+	@Produces(MediaType.TEXT_HTML)
 	public String broadcast() {
 		int machineResponses = 0;
 		ResteasyWebTarget[] machineTargets = Nodes.getOtherMachineTargets("/ppti/say-yes");
-		for (var machineTarget : machineTargets) {
-			Response response = machineTarget.request().get();
-			if (response.readEntity(String.class) == "yes")
-			machineResponses++;
+
+		YesRequestRunnable[] runnables = new YesRequestRunnable[machineTargets.length];
+		Thread[] requests = new Thread[machineTargets.length];
+		for (int i = 0; i < requests.length; i++) {
+			runnables[i] = new YesRequestRunnable(machineTargets[i]);
+			requests[i] = new Thread(runnables[i]);
+			requests[i].start();
 		}
+		
+		for (int i = 0; i < requests.length; i++) {
+			try {
+				requests[i].join();
+				if (runnables[i].hasResponded())
+					machineResponses++;
+			} catch (Exception e) {
+				System.err.println("ERROR: Request thread got interrupted: " + e);
+			}
+		}
+		
 		return "Number of nodes that responded: " + machineResponses;
+	}
+}
+
+class YesRequestRunnable implements Runnable {
+	private ResteasyWebTarget machineTarget;
+	private boolean hasResponded;
+
+	public YesRequestRunnable(ResteasyWebTarget machineTarget) {
+		this.machineTarget = machineTarget;
+		this.hasResponded = false;
+	}
+
+	public boolean hasResponded() {
+		return hasResponded;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Requesting to " + machineTarget.getUri());
+		try {
+			Response response = machineTarget.request().get();
+			if (response.readEntity(String.class) == "yes") {
+				System.out.println("They said yes! :D");
+				hasResponded = true;
+			} else {
+				System.out.println("They didn't say yes :(");
+				hasResponded = false;
+			}
+		} catch (Exception e) {
+			System.out.println("Failed: " + e);
+		}
 	}
 }
