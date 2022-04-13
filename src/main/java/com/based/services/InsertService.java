83fb44f2b9b9bd8e2285c23b87f6bcf82ac1681a
Speykious <speykious@gmail.com>
2022-04-14 01:13:32 +0200

    Basics of `InsertService`

--- src/main/java/com/based/services/InsertService.java
+++ src/main/java/com/based/services/InsertService.java
@@ -1,5 +1,20 @@
 package com.based.services;
 
+import java.io.InputStream;
+import java.util.List;
+
+import com.based.exception.MissingTableException;
+import com.based.model.Database;
+import com.based.model.Row;
+import com.based.model.Table;
+
 public class InsertService {
-    
+    public void insert(String tableName, List<Object> values) throws MissingTableException {
+        Table table = Database.getTable(tableName);
+        table.getStorage().add(new Row(values));
+    }
+
+    public void insertCsv(String tableName, InputStream csv) {
+
+    }
 }
