834f4861e14a054ff51744bc5ed75b3d31ce3fce
Speykious <speykious@gmail.com>
2022-04-13 17:53:35 +0200

    Add `primaryKey` and getters

--- src/main/java/com/based/model/Column.java
+++ src/main/java/com/based/model/Column.java
@@ -5,10 +5,23 @@ import java.io.Serializable;
 public class Column implements Serializable {
     private String name;
     private String type;
+    private boolean primaryKey;
     private boolean nullable;
 
-    public String getType(){
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
         return type;
+    }
+
+    public boolean isPrimaryKey() {
+        return primaryKey;
+    }
+
+    public boolean isNullable() {
+        return nullable;
     }
 
     @Override
