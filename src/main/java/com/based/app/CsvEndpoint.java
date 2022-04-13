e9cec204611608126de1aafbd0ee0d5bed45905d
Speykious <speykious@gmail.com>
2022-04-13 16:33:22 +0200

    Clean code rq

--- src/main/java/com/based/app/CsvEndpoint.java
+++ src/main/java/com/based/app/CsvEndpoint.java
@@ -1,6 +1,5 @@
 package com.based.app;
 
-import javax.swing.ScrollPaneLayout.UIResource;
 import javax.ws.rs.*;
 
 import java.io.BufferedReader;
@@ -11,23 +10,16 @@ import java.util.List;
 
 import com.based.entity.TableInfo;
 
-
 public class CsvEndpoint {
   @POST
   @Path("/csv")
-  public void parseCSV(InputStream inputstream, TableInfo table) throws IOException{
-    // lire le csv -> transformer dans list<list<string>>
+  public void importCsv(InputStream inputstream, TableInfo table) throws IOException {
     InputStreamReader input = new InputStreamReader(inputstream);
-    BufferedReader csvReader = new BufferedReader(input) ;
+    BufferedReader csvReader = new BufferedReader(input);
     String row;
     while ((row = csvReader.readLine()) != null) {
-      // csv -> table
-      /*
-      row -> <List<List<String>> 
-      */
       String[] data = row.split(",");
-      // mettre la contenu de fichier dans la Table
-    } 
+    }
     csvReader.close();
   }
 }
