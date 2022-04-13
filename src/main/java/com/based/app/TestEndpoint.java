69d4f759b3fa971fb0a396625633e375247a98c9
Speykious <speykious@gmail.com>
2022-04-13 13:54:30 +0200

    Revert `TestEndpoint` changes

--- src/main/java/com/based/app/TestEndpoint.java
+++ src/main/java/com/based/app/TestEndpoint.java
@@ -1,14 +1,12 @@
 package com.based.app;
 
+import java.util.List;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.based.entity.Account;
-import com.based.entity.Table;
-
-import java.io.InputStream;
-import java.util.List;
 
 @Path("/test")
 @Produces(MediaType.APPLICATION_JSON)
@@ -35,31 +33,9 @@ public class TestEndpoint {
 		return account;
 	}
 
-	@POST
-	@Path("/table")
-	public Table createTable(Table table) {
-		Database.putTable(table);
-		System.out.print(table + " is created");
-		return table;
-	}
-
 	@GET
 	@Path("/exception")
 	public Response exception() {
 		throw new RuntimeException("Cringe runtime error");
 	}
-
-	@POST
-	public String upload(InputStream input){
-		//Lire scv inoutStream
-
-		return "";
-	}
-
-	/**
-	 * new class endPOint 	createTable, uploadFichier, requete select
-	 * Stocker des strings	/	Select
-	 * 
-	 * list list string
-	 */
 }
