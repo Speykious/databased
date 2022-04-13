dddd5047fa1db5f756f759ef6731e8a6ed723646
Speykious <speykious@gmail.com>
2022-04-13 18:08:55 +0200

    Create `Table`

---
+++ src/main/java/com/based/model/Table.java
@@ -0,0 +1,16 @@
+package com.based.model;
+
+import com.based.entity.dto.TableDTO;
+
+public class Table {
+    private TableDTO dto;
+    private Storage storage;
+
+    public TableDTO getDTO() {
+        return dto;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+}
