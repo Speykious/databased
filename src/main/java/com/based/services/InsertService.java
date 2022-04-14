4ae5f1c4377a4b9674a20f13d05e2a80f1a347c4
Speykious <speykious@gmail.com>
2022-04-14 17:55:20 +0200

    Make `insertCsv` methods return nb of lines read

--- src/main/java/com/based/services/InsertService.java
+++ src/main/java/com/based/services/InsertService.java
@@ -18,20 +18,24 @@ public class InsertService {
         table.getStorage().add(new Row(values));
     }
 
-    public void insertCsv(String tableName, InputStream csv) throws IOException, MissingTableException {
-        insertCsv(tableName, csv, ",");
+    public int insertCsv(String tableName, InputStream csv) throws IOException, MissingTableException {
+        return insertCsv(tableName, csv, ",");
     }
 
-    public void insertCsv(String tableName, InputStream csv, String csvValueSplitter) throws IOException, MissingTableException {
+    public int insertCsv(String tableName, InputStream csv, String csvValueSplitter) throws IOException, MissingTableException {
         Table table = Database.getTable(tableName);
         BufferedReader csvReader = new BufferedReader(new InputStreamReader(csv));
 
+        int nbRows = 0;
         String csvRow;
         while ((csvRow = csvReader.readLine()) != null) {
             Object[] values = (Object[]) csvRow.split(csvValueSplitter);
             Row row = new Row(Arrays.asList(values));
             table.getStorage().add(row);
+            nbRows++;
         }
         csvReader.close();
+
+        return nbRows;
     }
 }
