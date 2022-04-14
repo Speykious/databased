569386f356c8c96dbc51a5bf4375c702c23d84d7
Speykious <speykious@gmail.com>
2022-04-14 16:34:22 +0200

    Add `InvalidTableDTOException`
    We can now manage all errors related to invalid TableDTOs :D

--- src/main/java/com/based/app/TableEndpoint.java
+++ src/main/java/com/based/app/TableEndpoint.java
@@ -5,6 +5,7 @@ import javax.ws.rs.core.MediaType;
 
 import com.based.entity.dto.TableDTO;
 import com.based.exception.DuplicateTableException;
+import com.based.exception.InvalidTableDTOException;
 import com.based.exception.MissingTableException;
 import com.based.model.Database;
 import com.based.model.Table;
@@ -14,7 +15,7 @@ import com.based.model.Table;
 @Consumes(MediaType.APPLICATION_JSON)
 public class TableEndpoint {
 	@POST
-	public String createTable(TableDTO request) throws DuplicateTableException {
+	public String createTable(TableDTO request) throws DuplicateTableException, InvalidTableDTOException {
 		Database.addTable(new Table(request));
 		return "Created table:\n" + request;
 	}
