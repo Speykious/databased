71e399fd77efcde0b4df1a474013737d2c7c2f56
Etienne Païta <etiennepaita1@gmail.com>
2022-05-09 17:02:04 +0200

    start where

--- src/main/java/com/based/services/SelectService.java
+++ src/main/java/com/based/services/SelectService.java
@@ -1,7 +1,6 @@
 package com.based.services;
 
 import java.util.List;
-
 import com.based.exception.MissingTableException;
 import com.based.model.Database;
 import com.based.model.Row;
@@ -27,4 +26,45 @@ public class SelectService {
     public List<Row> selectWhere(String tableName) throws MissingTableException {
         return Database.getTable(tableName).getStorage().getRows();
     }
+
+    //const myCondition = "column_name == 'bonjour' and ( id > 3 and desh != 'real' )"
+
+    //column
+
+    //[
+    //   { type: "ident", value: "column_name" },
+    //   { type: "operator", value: "==" },
+    //   { type: "string", value: "bonjour" },
+    //   { type: "operator", value: "and" },
+    //   { type: "bracket", value: "(" },
+    //   { type: "ident", value: "id" },
+    //   { type: "operator", value: ">" },
+    //   { type: "number", value: "3" },
+    //   { type: "operator", value: "and" },
+    //   { type: "ident", value: "desh" },
+    //   { type: "operator", value: "!=" },
+    //   { type: "string", value: "real" },
+    //   { type: "bracket", value: ")" },
+    // ]
+
+
+    public void parseWhereCondition(String condition) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        String json = gson.toJson(1);
+        int fromJsonInt = gson.fromJson(json, int.class);
+        System.out.println("1 -> " + fromJsonInt);
+        
+        String[] splitCondition = condition.split(" ");
+
+        for(int i= 0; i < splitCondition.length; i++){
+            if(i != splitCondition.length){
+
+            }
+        }
+
+
+    }
+
 }
