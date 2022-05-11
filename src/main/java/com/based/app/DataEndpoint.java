e994a543e8d1450a5e3f1194f9465ffc77a21fc8
Rashid <rashid.aliev@sp3d.co>
2022-05-11 18:19:39 +0200

    Adding some conditions to fix bug

--- src/main/java/com/based/app/DataEndpoint.java
+++ src/main/java/com/based/app/DataEndpoint.java
@@ -30,14 +30,23 @@ public class DataEndpoint {
 
 	@GET
 	@Path("/{tableName}")
-	public List<Row> select(@PathParam("tableName") String tableName, SelectRequestDTO selectRequest) throws MissingTableException, MissingColumnException {
+	public List<Row> select(@PathParam("tableName") String tableName, SelectRequestDTO selectRequest) throws MissingTableException, MissingColumnException, Exception {
 		System.out.println("Selecting some data from a table");
 		SelectService selectService = new SelectService();
+		
+		if(selectRequest != null) {
+			WhereCondition where = selectRequest.getWhere();
 
-		if(selectRequest == null) throw new MissingTableException(tableName);
-		WhereCondition where = selectRequest.getWhere();
+			if(where != null) {
+				System.out.println("where !!!!!!!!!!!!!!!!!!!!!!!!!!!");
+				return selectService.selectWhere(tableName, selectRequest);
+			}else{
+				return selectService.selectAll(tableName);
+			}
+		}
+		System.out.println("No REQUEST !!!!!!!!!");
 
-		if(where == null) return selectService.selectAll(tableName);
-		return selectService.selectWhere(tableName, selectRequest);
+		throw new Exception("Commande incorrect");
+
 	}
 }
