25c792b82f47ab8b315721179c38336447dc0d14
Speykious <speykious@gmail.com>
2022-05-18 17:34:24 +0200

    Generate `hashCode()` method in all entities

--- src/main/java/com/based/entity/NbLinesResponse.java
+++ src/main/java/com/based/entity/NbLinesResponse.java
@@ -12,4 +12,12 @@ public class NbLinesResponse implements Serializable {
     public int getNbLines() {
         return nbLines;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + nbLines;
+        return result;
+    }
 }
